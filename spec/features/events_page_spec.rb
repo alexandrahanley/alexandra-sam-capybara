20e1d3b7405faf2766d201d71dfa10ae4dbd7d9d
Alexandra Hanley <alexandrahanley@gmail.com>
2015-02-11 16:33:31 -0700

    add test to edit an event

--- spec/features/events_page_spec.rb
+++ spec/features/events_page_spec.rb
@@ -1,44 +1,61 @@
-# Users can create an event at an event.
-# Users can see a show page for an event.
-# Users can edit an event.
-# Users can delete an event.
 
 require 'rails_helper'
 
 describe 'User can CRUD events' do
 
-scenario 'User can view a show page for an event' do
-  @location = Location.create(:name => "g6", :address => "2062 delaware", :zipcode => 122121)
-  @event = Event.create(description: "Event1", date: "1/23/15", requires_id: "true", location_id: 22 )
+    scenario 'User can view a show page for an event' do
+        @location = Location.create(:name => "g6", :address => "2062 delaware", :zipcode => 122121)
+        @event = Event.create(description: "Event1", date: "1/23/15", requires_id: "true", location_id: 22 )
 
-  visit "locations/#{@location.id}/events/#{@event.id}"
-  expect(page).to have_content("Event1")
-end
-
-
+        visit "locations/#{@location.id}/events/#{@event.id}"
+        expect(page).to have_content("Event1")
+    end
 
 
-scenario 'User can create an event at a location' do
 
-@location = Location.create(:name => "galva", :address => "2015 haight", :zipcode => 1221)
-@event = Event.create(description: "Event1", date: "1/23/15", requires_id: "true", location_id: 22 )
 
-visit "/locations/#{@location.id}/"
+    scenario 'User can create an event at a location' do
 
-click_on ("New Event")
+        @location = Location.create(:name => "galva", :address => "2015 haight", :zipcode => 1221)
+        @event = Event.create(description: "Event1", date: "1/23/15", requires_id: "true", location_id: 22 )
 
-visit "/locations/#{@location.id}/events/new"
+        visit "/locations/#{@location.id}/"
 
-fill_in 'event[description]', :with => "Great event"
-fill_in 'event[date]', with: "1/23/15"
-check 'event[requires_id]', "checked"
+        click_on ("New Event")
 
-click_on ("Create Event")
+        visit "/locations/#{@location.id}/events/new"
 
-visit "/locations/#{@location.id}"
+        fill_in 'event[description]', :with => "Great event"
+        fill_in 'event[date]', with: "1/23/15"
+        check 'event[requires_id]', "checked"
 
-expect(page).to have_content("Great event")
+        click_on ("Create Event")
 
-end
+        visit "/locations/#{@location.id}"
+
+        expect(page).to have_content("Great event")
+
+    end
+
+
+    scenario 'User can edit an event' do
+
+        @location = Location.create(:name => "galva", :address => "2015 haight", :zipcode => 1221)
+        @event = Event.create(description: "Event1", date: "1/23/15", requires_id: "true", location_id: 22 )
+
+
+        visit "locations/#{@location.id}/events/#{@event.id}/edit"
+
+        fill_in 'event[description]', :with => "Shitty event"
+
+        click_on("Update Event")
+        expect(page).to have_content("Event was successfully updated")
+
+        visit "/locations/#{@location.id}/events/#{@event.id}"
+
+        expect(page).to have_content("Shitty event")
+
+    end
+
 
 end
