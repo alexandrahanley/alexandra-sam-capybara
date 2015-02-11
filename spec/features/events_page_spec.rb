614bc1106a1b090125e1231756b4b9111287b6b5
Alexandra Hanley <alexandrahanley@gmail.com>
2015-02-11 15:56:27 -0700

    Add test for user to create new event

--- spec/features/events_page_spec.rb
+++ spec/features/events_page_spec.rb
@@ -1,4 +1,3 @@
-# Users can visit a show page for an event.
 # Users can create an event at an event.
 # Users can see a show page for an event.
 # Users can edit an event.
@@ -19,5 +18,27 @@ end
 
 
 
+scenario 'User can create an event at a location' do
+
+@location = Location.create(:name => "galva", :address => "2015 haight", :zipcode => 1221)
+@event = Event.create(description: "Event1", date: "1/23/15", requires_id: "true", location_id: 22 )
+
+visit "/locations/#{@location.id}/"
+
+click_on ("New Event")
+
+visit "/locations/#{@location.id}/events/new"
+
+fill_in 'event[description]', :with => "Great event"
+fill_in 'event[date]', with: "1/23/15"
+check 'event[requires_id]', "checked"
+
+click_on ("Create Event")
+
+visit "/locations/#{@location.id}"
+
+expect(page).to have_content("Great event")
+
+end
 
 end
